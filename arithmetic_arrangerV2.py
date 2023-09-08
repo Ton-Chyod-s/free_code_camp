05f3da141cecfa1f2122e7a6590734f4e50dbaf6
Klay <hix_x@hotmail.com>
2023-09-08 13:31:21 -0400

    doc: att

---
+++ arithmetic_arrangerV2.py
@@ -0,0 +1,39 @@
+def arithmetic_arranger(problems, resposta=False):
+  res = []
+  qtde = len(problems)
+  contas = []
+  for linha in problems:
+    valor1 = linha.split()
+    for i in range(3):
+      valor2 = valor1[i]
+      res.append(valor2)
+
+  for lin in range(qtde):
+    indice = lin * 3
+    linha1 = res[indice]
+    linha2 = res[indice + 1] +' '+ res[indice + 2]
+    valo1 = int(res[indice])
+    valo3 = int(res[indice + 2])
+
+    operadores = {
+      '+': lambda x, y: x + y,
+      '-': lambda x, y: x - y,
+      '*': lambda x, y: x * y,
+      '/': lambda x, y: x / y
+    }
+
+    if resposta:
+      if res[indice + 1] in operadores:
+          resposta = operadores[res[indice + 1]](valo1, valo3)
+    else:
+      resposta = ''
+
+    conta = f'{linha1}\n{linha2}\n-----\n{resposta}'
+    
+    contas.append(conta)
+
+  print(contas[0]+ contas[1])
+
+if __name__ == '__main__':
+  arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
+  arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True)
