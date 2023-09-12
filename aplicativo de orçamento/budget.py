cd938adc162971f64a0478cd70fc592d0f37872e
Klay <hix_x@hotmail.com>
2023-09-12 16:36:15 -0400

    doc: att

---
+++ aplicativo de orçamento/budget.py
@@ -0,0 +1,19 @@
+class Category:
+    def __init__(self, txt):
+        self.categoria = txt
+        self.ledger = []
+
+    def deposito(self):
+        pass
+
+    def sacar(sef):
+        pass
+    
+    def balanco(self):
+        pass
+    
+    def transferencia(self):
+        pass
+
+def create_spend_chart(categories):
+    pass
