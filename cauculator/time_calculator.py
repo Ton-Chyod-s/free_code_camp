f4e51782a7b139f943101cd625b66fb90834a5a9
Klay <hix_x@hotmail.com>
2023-09-12 09:05:35 -0400

    doc: att

--- cauculator/time_calculator.py
+++ cauculator/time_calculator.py
@@ -21,32 +21,44 @@ def add_time(start, duration):
     soma_minuto  = minuto_inicio + minuto_duracao   
 
     if hora_duracao >= 24:
-        # dias 
-        dia = int(hora_duracao / 24) + 1
-        dias_extras = hora_duracao / 24
-        minutos_extras = dias_extras % 1
-        minutos_extras = str(round(minutos_extras,2)).split('.')
-        minutos_extras = int(minutos_extras[1])
+        # dias
+        dia = hora_duracao / 24
+        if dia % 1 != 0.0:
+           dia = int(hora_duracao / 24) + 1
         
+        dias_extras = float(hora_duracao / 24)
+        minutos_extras = (dias_extras % 1) 
+        # minutos_extras = str(round(minutos_extras,2)).split('.')
+        # minutos_extras = int(minutos_extras[1])
+        
+        minutos_extras = minutos_extras * 60
         hora_duracao = 0
         soma_hora = hora_inicio + hora_duracao
         
+
     # Verificar se os minutos ultrapassam 60 e ajustar as horas e minutos
     if soma_minuto >= 60:
         soma_hora += 1
         soma_minuto -= 60
-
-    resultado_hora = "{:02d}:{:02f}".format(soma_hora, soma_minuto)
+    
+    resultado_hora = "{}:{:02d}".format(soma_hora, soma_minuto)
     # Dividir a hora formatada em horas e minutos
     hora_ = resultado_hora.split(':')
     hora_ = int(hora_[0])
 
-    if hora_ < 12:
-       print(F'{resultado_hora} AM') 
-    
+    if hora_ <= 12:
+        if dia > 2:
+            print(F'{resultado_hora} AM ({dia} days later)')
+        elif dia == 1:
+            print(F'{resultado_hora} AM (next day)')
+        else:
+            print(F'{resultado_hora} AM') 
+
     elif hora_ < 18:
        print(F'{resultado_hora} PM') 
 
 if __name__ == '__main__':
     add_time("6:30 PM", "205:12")
     # Returns: 7:42 AM (9 days later)
+    add_time("11:43 PM", "24:20")
+    # Returns: 12:03 AM, Thursday (2 days later)
