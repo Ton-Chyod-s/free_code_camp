0af545721b90e4a09a6373fd3fb7799b911530a4
Klay <hix_x@hotmail.com>
2023-09-12 16:52:29 -0400

    doc: att

--- aplicativo de orçamento/budget.py
+++ aplicativo de orçamento/budget.py
@@ -1,19 +1,23 @@
 class Category:
-    def __init__(self, txt):
-        self.categoria = txt
+    def __init__(self, category):
+        self.category = category
         self.ledger = []
 
-    def deposito(self):
-        pass
-
-    def sacar(sef):
-        pass
-    
-    def balanco(self):
+    def deposit(self,qtde,descricao):
+        self.ledger.append({"quantidade": qtde, "Descrição":descricao})
+        lol = lol
+    def withdraw(sef):
         pass
     
-    def transferencia(self):
+    def get_balance(self):
+        pass
+    
+    def transfer(self):
         pass
 
 def create_spend_chart(categories):
     pass
+
+if __name__ == '__main__':
+    food = Category("Food")
+    food.deposit(1000, "initial deposit")
