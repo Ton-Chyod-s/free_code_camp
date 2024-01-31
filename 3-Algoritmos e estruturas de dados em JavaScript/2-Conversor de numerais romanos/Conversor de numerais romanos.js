d11ebfd8bf103fd5bed129c505864789403c7351
Klay <arqkdias@gmail.com>
2024-01-31 16:46:19 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/2-Conversor de numerais romanos/Conversor de numerais romanos.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/2-Conversor de numerais romanos/Conversor de numerais romanos.js
@@ -1,2 +1,30 @@
+function convertToRoman(num) {
+    const roman = {
+        m: 1000,
+        cm: 900,
+        d: 500,
+        cd: 400,
+        c: 100,
+        xc: 90,
+        l: 50,
+        xl: 40,
+        x: 10,
+        ix: 9,
+        v: 5,
+        iv: 4,
+        i: 1
+    }
 
- 
+    for (let i in roman){
+        if (num === roman[i]) {
+            console.log(roman[i])
+        }
+        
+    }
+    
+    return num;
+   }
+   
+   convertToRoman(1); // IXLVI
+
+   
