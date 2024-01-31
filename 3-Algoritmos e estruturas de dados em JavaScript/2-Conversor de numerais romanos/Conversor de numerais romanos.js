b96380bc8038aed46511e22f2d774954bebb273d
Klay <arqkdias@gmail.com>
2024-01-31 16:09:50 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/2-Conversor de numerais romanos/Conversor de numerais romanos.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/2-Conversor de numerais romanos/Conversor de numerais romanos.js
@@ -1,22 +1,2 @@
-function palindrome(str) {
-    let minuscula = str.toLowerCase()
-    let semEspaço = minuscula.replace(/[^A-Za-z0-9]/g, '')
-    console.log(semEspaço)
-    const palindromo = new Array();
 
-    for (let i = 0; i < semEspaço.length; i++) {
-        palindromo.unshift(semEspaço[i]);
-        
-    }
-    console.log(palindromo.join(''))
-
-    if (semEspaço === palindromo.join('')) {
-        return true;
-    } else {
-        return false;
-    }
-    
-  }
-  
-  palindrome("race car")
  
