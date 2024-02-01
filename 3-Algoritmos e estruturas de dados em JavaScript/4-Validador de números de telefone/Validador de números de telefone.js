1060c9901fd6a5fb87f60a1eebd4dfb008240fb9
Klay <arqkdias@gmail.com>
2024-02-01 15:51:57 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/4-Validador de números de telefone/Validador de números de telefone.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/4-Validador de números de telefone/Validador de números de telefone.js
@@ -1,6 +1,6 @@
 function telephoneCheck(str) {
     const strLista = str.replace(/-/g," ").split(" ");
-    const verificador = /(\d{1,2})?[-.\s]?(\d{3})[-.\s]?(\d{3})[-.\s]?(\d{4})$/g;
+    const verificador = /(\d{1,2})?[-.\s]?[\W]?(\d{3})[\W]?[-.\s]?(\d{3})[-.\s]?(\d{4})$/g;
     const res = verificador.test(str);
     if (res) {
         return true
