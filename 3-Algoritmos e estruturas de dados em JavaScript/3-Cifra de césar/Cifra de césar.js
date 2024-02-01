ef4e6bdb80c100f0dacd13a49ef2304b374acb1d
Klay <arqkdias@gmail.com>
2024-02-01 09:46:27 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/3-Cifra de césar/Cifra de césar.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/3-Cifra de césar/Cifra de césar.js
@@ -1,6 +1,6 @@
 function rot13(str) {
     const strLista = str.toLowerCase().split("");
-    const cifraROT13 = { a: 'n', b: 'o', c: 'p', d: 'q', e: 'r', f: 's', g: 't', h: 'u', i: 'v', j: 'w', k: 'x', l: 'y', m: 'z' };
+    let cifraROT13 = { a: 'n', b: 'o', c: 'p', d: 'q', e: 'r', f: 's', g: 't', h: 'u', i: 'v', j: 'w', k: 'x', l: 'y', m: 'z' };
     let res = "";
     for (let i = 0; i < strLista.length; i++) {
         if (strLista[i] == " ") {
@@ -12,11 +12,28 @@ function rot13(str) {
                 break
             }
         }
+        function Inverter(obj) {
+            var retobj = {};
+            for (var key in obj) {
+                const newKey = obj[key]
+                retobj[newKey] = key;
+            }
+            return retobj
+        }
+
+        cifraROT13 = Inverter(cifraROT13)
+        
+        for (let j in cifraROT13) {
+            if (strLista[i] === cifraROT13[j]) {
+                res += j
+                break
+            }
+        }
     }
-    return str;
-    
+
+    return res;
   }
   
-  rot13("SERR PBQR PNZC");
+  console.log(rot13("SERR CVMMN!"))
 
  
