2a14829913530a75b3d410c49db2b80993e3e5f2
Klay <arqkdias@gmail.com>
2024-02-02 11:23:08 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
@@ -1,34 +1,26 @@
 function checkCashRegister(price, cash, cid) {
     let change = new Array();
-    const calc = cash - price;
-    if (calc === 0) {
+    let calculo = cash - price;
+    let valorTotalCid = 0;
+    for (let i in cid) {
+      valorTotalCid += cid[i][1]
+    }
+    if (calculo > valorTotalCid) {
+      return {status: "INSUFFICIENT_FUNDS", change}
+    } else if (calculo === 0) {
       return {status: "CLOSED", change}
-    } else if (calc > 0) {
-      const unidadesEmMoedas = [ "PENNY", .01, "NICKEL", .05, "DIME", .10, "QUARTER", .25, "ONE", 1, "FIVE", 5, "TEN", 10,"TWENTY", 20, "ONE HUNDRED", 100 ];
-      for (let i = 0; i < unidadesEmMoedas.length; i += 2) {
-        const elementoUnidadesEmMoedas = unidadesEmMoedas[i]
-        const j = i
-        let proximovalorUnidadesEmMoedas;
-        proximovalorUnidadesEmMoedas = unidadesEmMoedas[j + 3]
-        if (calc <= proximovalorUnidadesEmMoedas) {
-          res = change.push([elementoUnidadesEmMoedas,calc])
-          break
+    } else {
+      const valorUnidadeDeMoeda = { "Penny": .01, "Nickel": .05, "Dime": .1, "Quarter": .25, "Dollar": 1, "Five Dollars": 5, "Ten Dollars": 10, "Twenty Dollars": 20, "One-hundred Dollars": 100 }
+      for (let i in cid) {
+        const valorAtual = cid[i][1]
+        if () {
+          
         }
       }
+
       return {status: "OPEN", change}
-    } else {
-      return {status: "INSUFFICIENT_FUNDS", change}
     }
+
   }
   
-  console.log(checkCashRegister(19.5, 20, [
-    ["PENNY", 1.01], 
-    ["NICKEL", 2.05], 
-    ["DIME", 3.1], 
-    ["QUARTER", 4.25], 
-    ["ONE", 90], 
-    ["FIVE", 55], 
-    ["TEN", 20], 
-    ["TWENTY", 60], 
-    ["ONE HUNDRED", 100]
-  ]));
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
--- lol.js
+++ lol.js
@@ -1,18 +1,29 @@
-function rot13(str) {
-    
-    var alphabets =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'," ", "-", "_", ".", "&","?", "!", "@", "#", "/"];
-    
-    var alphabets13 = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M', " ", "-", "_", ".", "&","?", "!", "@", "#", "/"];
-    
-    var resultStr = [];
-    for(let i=0; i<str.length; i++){
-        for(let j =0; j<alphabets.length; j++){
-            if(str[i] === alphabets[j]){
-            resultStr.push(alphabets13[j]);
-            }
-        }
+const checkCashRegister = (price, cash, cid) => {
+    const UNIT_AMOUNT = {
+      "PENNY": .01,
+      "NICKEL": .05,
+      "DIME": .10,
+      "QUARTER": .25,
+      "ONE": 1.00,
+      "FIVE": 5.00,
+      "TEN": 10.00,
+      "TWENTY": 20.00,
+      "ONE HUNDRED": 100.00
     }
-    return resultStr.join("");
-  };
+    let totalCID = 0;
+    for (let element of cid) {
+      totalCID += element[1];
+    }
 
-  console.log(rot13("SERR CVMMN!"));
+    totalCID = totalCID.toFixed(2);
+    let changeToGive = cash - price;
+    const changeArray = [];
+
+    cid = cid.reverse();
+    const lool ='lol'
+    }
+
+
+
+
+    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
