fd475bcc7a77eb690431cc9a347230315eb36c03
Klay <arqkdias@gmail.com>
2024-02-02 14:53:07 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
@@ -2,25 +2,34 @@ function checkCashRegister(price, cash, cid) {
     let change = new Array();
     let calculo = cash - price;
     let valorTotalCid = 0;
+    let count = 0;
     for (let i in cid) {
-      valorTotalCid += cid[i][1]
+      if (count >= 3 && calculo > valorTotalCid) {
+        break
+      } else {
+        valorTotalCid += cid[i][1];
+      }
+      count++;
     }
     if (calculo > valorTotalCid) {
       return {status: "INSUFFICIENT_FUNDS", change}
-    } else if (calculo === 0) {
+    } else if (calculo === 0 || calculo === valorTotalCid) {
+      change = [...cid]
       return {status: "CLOSED", change}
     } else {
       const valorUnidadeDeMoeda = { "Penny": .01, "Nickel": .05, "Dime": .1, "Quarter": .25, "Dollar": 1, "Five Dollars": 5, "Ten Dollars": 10, "Twenty Dollars": 20, "One-hundred Dollars": 100 }
+      let res = 0;
       for (let i in cid) {
         const valorAtual = cid[i][1]
-        if () {
-          
-        }
+        if (calculo > valorAtual) {
+          console.log("lol")
+        } 
       }
+      
 
       return {status: "OPEN", change}
     }
 
   }
   
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+  console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
