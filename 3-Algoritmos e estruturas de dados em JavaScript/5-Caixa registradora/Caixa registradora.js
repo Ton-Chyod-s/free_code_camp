383b8dd2bd1465bcce2bf0b28d5d3d9740ba1818
Klay <arqkdias@gmail.com>
2024-02-02 08:30:06 -0400

    doc: att

--- 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
+++ 3-Algoritmos e estruturas de dados em JavaScript/5-Caixa registradora/Caixa registradora.js
@@ -1,8 +1,24 @@
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change = cash - price;
+    if (change === 0) {
+      return {status: "CLOSED", change}
+    } else if (change > 0) {
+      const unidadesEmMoedas = { "PENNY": .01, "NICKEL": .05, "DIME": .10, "QUARTER": .25, "ONE": 1, "FIVE": 5, "TEN": 10,"TWENTY": 20, "ONE HUNDRED": 100 }
 
-    
-    return change;
+      return {status: "OPEN", change}
+    } else {
+      return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  console.log(checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01], 
+    ["NICKEL", 2.05], 
+    ["DIME", 3.1], 
+    ["QUARTER", 4.25], 
+    ["ONE", 90], 
+    ["FIVE", 55], 
+    ["TEN", 20], 
+    ["TWENTY", 60], 
+    ["ONE HUNDRED", 100]
+  ]));
