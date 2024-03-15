e98aa13266138f93853d543e175aa1fed7810ad4
Klay <arqkdias@gmail.com>
2024-03-15 14:08:55 -0400

    doc: att

--- 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
+++ 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
@@ -107,14 +107,9 @@ def create_spend_chart(categories):
 if __name__ == '__main__':
     
     food = Category("Food")
-    food.deposit(900, "deposit")
-    food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-    food.transfer(20, Category('entertainment'))
+    food.deposit(1000, "deposit")
+    food.withdraw(10.15, "groceries")
+    food.withdraw(15.89, "restaurant and more food for dessert")
+    clothing = Category("Clothing")
+    food.transfer(50, clothing)
     print(food)
-
-
-
-
-
-    expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
-    print(expected)
