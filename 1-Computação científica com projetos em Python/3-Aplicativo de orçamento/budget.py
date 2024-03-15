9e7afd09801506698c7755cc0159ae5b41074b76
Klay <hix_x@hotmail.com>
2024-03-15 19:13:09 -0400

    doc: att

--- 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
+++ 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
@@ -111,7 +111,7 @@ def create_spend_chart(categories):
         categoria = category.category
         balanco = category.balance
         vendas = category.withdraw_deposit
-        calc = round((vendas * -1 / balanco) * 100, 2)
+        calc = round((vendas * -1 / balanco) *100, 2)
         percentual_cat[categoria] = calc
 
         # Adicionar a palavra da categoria formatada
@@ -163,5 +163,5 @@ if __name__ == '__main__':
     create_spend_chart([business, food, entertainment])
 
 
-    """expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-    print(expected)"""
+    expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+    print(expected)
