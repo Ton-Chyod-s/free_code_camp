5f59808ecca7a81fb340ae16e84ab846796c21a7
Klay <arqkdias@gmail.com>
2024-03-15 16:33:05 -0400

    doc: att

--- 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
+++ 1-Computação científica com projetos em Python/3-Aplicativo de orçamento/budget.py
@@ -157,5 +157,5 @@ if __name__ == '__main__':
     create_spend_chart([business, food, entertainment])
 
 
-    expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
-    print(expected)
+    """expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+    print(expected)"""
