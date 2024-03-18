12a274df90e331cfcdf3b49057a207ee9d681261
Klay <arqkdias@gmail.com>
2024-03-18 11:20:10 -0400

    doc: att

--- 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
+++ 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
@@ -2,7 +2,16 @@ import copy
 import random
 
 class Hat:
-    pass
+    def __init__(self,*args):
+        print(*args)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
+
+
+if __name__ == '__main__':
+    hat = Hat(black=6, red=4, green=3)
+    probability = experiment(hat=hat,
+                    expected_balls={"red":2,"green":1},
+                    num_balls_drawn=5,
+                    num_experiments=2000)
