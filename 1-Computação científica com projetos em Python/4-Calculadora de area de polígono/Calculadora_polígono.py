160cc6da4e76bd29f37231e3d79d821b6ee8b39e
Klay <arqkdias@gmail.com>
2024-03-18 07:37:04 -0400

    doc: att

--- 1-Computação científica com projetos em Python/4-Calculadora de area de polígono/Calculadora_polígono.py
+++ 1-Computação científica com projetos em Python/4-Calculadora de area de polígono/Calculadora_polígono.py
@@ -1,5 +1,10 @@
 class Rectangle:
-    pass
+    def __init__(self,width,height)
+        self.width = width
+        self.height = height
 
 class Square:
     pass
+
+if __name__ == "__init__":
+    rect = Rectangle(10, 5)
