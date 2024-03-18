34c74267229d501acdb969a2f7519067849c078f
Klay <arqkdias@gmail.com>
2024-03-18 14:39:00 -0400

    doc: att

--- 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
+++ 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
@@ -2,15 +2,30 @@ import copy
 import random
 
 class Hat:
-    def __init__(self,*args):
-        print(*args)
+    def __init__(self,**kwargs):
+        self.contents  = {}
+        for color, quantity in kwargs.items():
+            self.contents[color] = quantity
+
+    def draw(self,arg):
+        color = list(self.contents.keys())
+        coresSelecionadas = []
+        for i in range(0,len(self.contents)):
+            bola_Retiradas = random.randint(0,arg)
+            corSelecionada = color[bola_Retiradas - 1]
+            novoValor = (self.contents[corSelecionada]) - 1
+            self.contents[corSelecionada] = novoValor
+            coresSelecionadas.append(corSelecionada)
+        return coresSelecionadas
+        
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
-
-
+    hat = hat
+  
 if __name__ == '__main__':
     hat = Hat(black=6, red=4, green=3)
+    print(hat.draw(3))
+
     probability = experiment(hat=hat,
                     expected_balls={"red":2,"green":1},
                     num_balls_drawn=5,
