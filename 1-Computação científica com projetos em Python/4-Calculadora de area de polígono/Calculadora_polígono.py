ad55ac006f9dd6ed969eafd69a4a5cd0d0682e3c
Klay <arqkdias@gmail.com>
2024-03-18 11:00:44 -0400

    doc: att

--- 1-Computação científica com projetos em Python/4-Calculadora de area de polígono/Calculadora_polígono.py
+++ 1-Computação científica com projetos em Python/4-Calculadora de area de polígono/Calculadora_polígono.py
@@ -64,5 +64,17 @@ if __name__ == '__main__':
     rect = Rectangle(10, 5)
     print(rect.get_area())
     rect.set_height(3)
-    rect.set_width(7)
+    print(rect.get_perimeter())
     print(rect)
+    print(rect.get_picture())
+
+    sq = Square(9)
+    print(sq.get_area())
+    sq.set_side(4)
+    print(sq.get_diagonal())
+    print(sq)
+    print(sq.get_picture())
+
+    rect.set_height(8)
+    rect.set_width(16)
+    print(rect.get_amount_inside(sq))
