da8cf4a8e8b4874ef7864b93af3cb51253388b0c
Klay <arqkdias@gmail.com>
2024-03-18 11:02:07 -0400

    doc: att

--- 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
+++ 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
@@ -1,9 +1,8 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
+    pass
 
-
-    def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-        pass
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
