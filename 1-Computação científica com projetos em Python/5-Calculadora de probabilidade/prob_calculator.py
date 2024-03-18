4e18a8a88bff9ccbb43f9958b5dd82c5c3f6f20f
Klay <arqkdias@gmail.com>
2024-03-18 15:31:37 -0400

    doc: att

--- 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
+++ 1-Computação científica com projetos em Python/5-Calculadora de probabilidade/prob_calculator.py
@@ -3,28 +3,36 @@ import random
 
 class Hat:
     def __init__(self,**kwargs):
-        self.contents  = {}
+        self.dictContents  = {}
         for color, quantity in kwargs.items():
-            self.contents[color] = quantity
+            self.dictContents[color] = quantity
+        self.contents = list(self.dictContents.keys())
 
     def draw(self,arg):
-        color = list(self.contents.keys())
         coresSelecionadas = []
-        for i in range(0,len(self.contents)):
-            bola_Retiradas = random.randint(0,arg)
-            corSelecionada = color[bola_Retiradas - 1]
-            novoValor = (self.contents[corSelecionada]) - 1
-            self.contents[corSelecionada] = novoValor
+        bolasSelecionadas = {}
+        for i in range(0,arg):
+            bola_Retiradas = random.randint(0,len(self.dictContents))
+            corSelecionada = self.contents[bola_Retiradas - 1]
+            novoValor = (self.dictContents[corSelecionada]) - 1
+            self.dictContents[corSelecionada] = novoValor
             coresSelecionadas.append(corSelecionada)
+            bolasSelecionadas[corSelecionada] =  1
         return coresSelecionadas
         
+    def __str__(self):
+        return self.dictContents
+    
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    hat = hat
-  
+    if hat == expected_balls:
+        pass
+    else:
+        pass
+
 if __name__ == '__main__':
     hat = Hat(black=6, red=4, green=3)
-    print(hat.draw(3))
+    print(hat.draw(5))
 
     probability = experiment(hat=hat,
                     expected_balls={"red":2,"green":1},
