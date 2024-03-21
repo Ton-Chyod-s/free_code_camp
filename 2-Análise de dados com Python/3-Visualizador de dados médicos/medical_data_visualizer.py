a4e09565543b34669c51aed1a41e8f0b0712b825
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-21 07:40:18 -0400

    doc: att

--- 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
+++ 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import os
 
-df = pd.read_csv(os.path.abspath('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.csv'))
+df = pd.read_csv(os.path.abspath('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.csv'), encoding='utf-8')
 
