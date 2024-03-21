a3dac70a9a70d59ecc9ab21347bd2ab788a44803
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-21 08:41:44 -0400

    dpc: att

--- 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
+++ 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import os
 
-df = pd.read_csv(os.path.abspath('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.csv'), encoding='utf-8')
+df = pd.read_excel(os.path.abspath('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.xlsx'))
+print(df)
 
+#nova coluna
+imc = 
+overweight = 
