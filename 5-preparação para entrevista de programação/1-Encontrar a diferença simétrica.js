be4c7660d7aa39251826f6f1cc02d5a6440276c3
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-13 16:55:30 -0400

    doc: att

--- 5-preparação para entrevista de programação/1-Encontrar a diferença simétrica.js
+++ 5-preparação para entrevista de programação/1-Encontrar a diferença simétrica.js
@@ -1,20 +1,20 @@
 function sym(args) {
     args = Array.from(arguments);
     let result = [];
-    let tm = 0;
+    let tamanho = 0;
 
     for (let d = 0; d < args.length; d++) {
+        if (tamanho != 0) {
+            tamanho = tamanho - 1;
+        } else {
+            tamanho = args.length - 1;
+        }
         for (let i = 0; i < args[d].length; i++) {
             let element = args[d][i];
-            let tamanho = args.length - 1;
-            if (tm != 0) {
-                let tamanho = tamanho + tm;
-            }
             if (!args[tamanho].includes(element)) {
                 result.push(element);
             };
         }
-        tm -= 1;
     }   
     return result;
   }
