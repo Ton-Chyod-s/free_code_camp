5c9451ec1262d1a004445d74ebe25e0c3c3c84ed
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-14 17:16:26 -0400

    doc: att

--- 5-preparação para entrevista de programação/1-Encontrar a diferença simétrica.js
+++ 5-preparação para entrevista de programação/1-Encontrar a diferença simétrica.js
@@ -1,21 +1,13 @@
 function sym(args) {
     args = Array.from(arguments);
     let result = [];
-    let tamanho = 0;
-
+    
     for (let d = 0; d < args.length; d++) {
-        if (tamanho != 0) {
-            if (tamanho === 1) {
-                tamanho = tamanho - 1;
-            } else if (tamanho === 2) {
-                tamanho = tamanho - 2;
-            } else if (tamanho === 3) {
-                tamanho = tamanho - 3;
-            }
-            
-        } else {
-            tamanho = args.length - 1;
-        }
+        let tamanho = d + 1;
+        if (d == args.length - 1) {
+            break;
+        } 
+
         for (let i = 0; i < args[d].length; i++) {
             let element = args[d][i];
             if (!args[tamanho].includes(element)) {
@@ -28,4 +20,4 @@ function sym(args) {
     return result;
   }
   
-console.log(sym([1, 2, 5], [2, 3, 5], [3, 4, 5]));
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
