fc1b5247cf58f0634d858465fe0dac243c0142e9
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-15 16:38:30 -0400

    doc: att

--- 4-Bancos de dados relacionais/2-Criar um banco de dados da Copa do Mundo/insert_data.sh
+++ 4-Bancos de dados relacionais/2-Criar um banco de dados da Copa do Mundo/insert_data.sh
@@ -11,24 +11,20 @@ fi
 
 echo "$($PSQL "TRUNCATE TABLE games, teams")"
 
-# Read the CSV file line by line
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS; 
 do
     TEAM_ID_WINNER=$($PSQL "SELECT name FROM teams WHERE name = '$WINNER'")
-    # if not found winner
     if [[ -z $TEAM_ID_WINNER && $WINNER != 'winner' ]] 
     then
       echo "$($PSQL "INSERT INTO teams(name) values('$WINNER')")"
     fi
 
     TEAM_ID_OPPONENT=$($PSQL "SELECT name FROM teams WHERE name = '$OPPONENT'")
-    # if not found opponent
     if [[ -z $TEAM_ID_OPPONENT && $OPPONENT != 'opponent' ]] 
     then
       echo "$($PSQL "INSERT INTO teams(name) values('$OPPONENT')")"
     fi
 
-    #obtendo id vencedor e oponente
     TEAM_ID_WINNER="$($PSQL "SELECT team_id from teams where name = '$WINNER'")"
     TEAM_ID_OPPONENT="$($PSQL "SELECT team_id from teams where name = '$OPPONENT'")"
 
