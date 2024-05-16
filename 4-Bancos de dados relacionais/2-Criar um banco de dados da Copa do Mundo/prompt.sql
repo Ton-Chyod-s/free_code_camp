0f9778a1810bbdd7449d10041e3607d725461e38
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-16 14:19:15 -0400

    doc: att

--- 4-Bancos de dados relacionais/2-Criar um banco de dados da Copa do Mundo/prompt.sql
+++ 4-Bancos de dados relacionais/2-Criar um banco de dados da Copa do Mundo/prompt.sql
@@ -10,40 +10,22 @@ psql --username=freecodecamp --dbname=postgres
 CREATE DATABASE worldcup;
 \c worldcup
 
-CREATE TABLE teams();
-CREATE TABLE games();
-
-ALTER TABLE teams 
-ADD COLUMN team_id SERIAL PRIMARY KEY;
-
-ALTER TABLE teams 
-ADD COLUMN name VARCHAR NOT NULL UNIQUE;
-
-ALTER TABLE games 
-ADD COLUMN game_id SERIAL PRIMARY KEY;
-
-ALTER TABLE games 
-ADD COLUMN year INT NOT NULL;
-
-ALTER TABLE games 
-ADD COLUMN round VARCHAR NOT NULL;
-
-ALTER TABLE games 
-ADD COLUMN winner_id INT NOT NULL;
-
-ALTER TABLE games 
-ADD COLUMN opponent_id INT NOT NULL;
-
-ALTER TABLE games 
-ADD COLUMN winner_goals INT NOT NULL;
-
-ALTER TABLE games 
-ADD COLUMN opponent_goals INT NOT NULL;
-
-ALTER TABLE games
-ADD CONSTRAINT fk_winner_id FOREIGN KEY (winner_id) REFERENCES teams(team_id);
-
-ALTER TABLE games
-ADD CONSTRAINT fk_opponent_id FOREIGN KEY (opponent_id) REFERENCES teams(team_id);
+CREATE TABLE teams(
+    team_id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE
+);
+      
+CREATE TABLE games(
+    game_id SERIAL PRIMARY KEY,
+    year INT NOT NULL,
+    round VARCHAR NOT NULL,
+    winner_id INT NOT NULL,
+    opponent_id INT NOT NULL,
+    winner_goals INT NOT NULL,
+    opponent_goals INT NOT NULL,
+      
+    FOREIGN KEY (opponent_id) REFERENCES teams(team_id),
+    FOREIGN KEY (winner_id) REFERENCES teams(team_id)
+);
 
 */
