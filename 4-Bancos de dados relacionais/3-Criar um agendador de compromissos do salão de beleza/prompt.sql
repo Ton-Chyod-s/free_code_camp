34bcd6fc550dc055b1f143223fc292f26e6bc5e1
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-29 16:56:34 -0400

    doc: att

---
+++ 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/prompt.sql
@@ -0,0 +1,25 @@
+create database salon;
+
+/* \c salon; */
+
+create table customers(
+  customer_id serial primary key,
+  phone varchar unique
+
+
+);
+
+create table appointments(
+  appointment_id serial primary key,
+  customer_id int not null,
+  service_id int not null,
+  name varchar not null,
+  time varchar
+
+);
+
+create table services(
+  service_id serial primary key,
+  name varchar not null
+
+);
