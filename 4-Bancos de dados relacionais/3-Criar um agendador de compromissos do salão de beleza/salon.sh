72874fa8790648345caf49381ab8036664ef0fa4
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-31 13:30:44 -0400

    doc: ATT

--- 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/salon.sh
+++ 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/salon.sh
@@ -19,20 +19,43 @@ MAIN_MENU() {
       echo "$SERVICE_ID) $NAME"
     fi
   done
-  SERVICES_COUNT=$(echo "$SERVICES" | grep -v "rows" | wc -l)
+  SERVICES_COUNT=$(($(echo "$SERVICES" | wc -l) - 3))
 
   read MAIN_MENU_SELECTION
 
-  # Validate user input
-  if [[ $MAIN_MENU_SELECTION -gt $SERVICES_COUNT ]]
-  then
-    echo "lol"
-  else
-    echo -e "\nI could not find that service. What would you like today?"
-    MAIN_MENU
-  fi
 
-}
+  if [[ $MAIN_MENU_SELECTION -gt 0 && $MAIN_MENU_SELECTION -le $SERVICES_COUNT ]]
+    then
+      echo "You selected service ID: $MAIN_MENU_SELECTION"
+    else
+      echo -e "\nI could not find that service. What would you like today?"
+      MAIN_MENU
+    fi
+
+
+
+  # # Validate user input
+  # if [[ $MAIN_MENU_SELECTION -gt $SERVICES_COUNT ]]
+  # then
+  #   echo -e "\nI could not find that service. What would you like today?"
+  #   MAIN_MENU
+  # else
+  #   echo -e "\nWhat's your phone number?"
+  #   read PHONE
+
+  #   EXIST_PHONE=$($PSQL "select name from customers where phone = $PHONE")
+  #   if [[ ! -z $EXIST_PHONE ]]
+  #   then
+  #     echo -e "\nWhat time would you like your cut, Fabio?"
+  #     read TIME
+  #   else
+  #     echo -e "\nI don't have a record for that phone number, what's your name?"
+  #     read NEW_NAME
+  #   fi
+
+
+  
+  }
 
 
 MAIN_MENU
