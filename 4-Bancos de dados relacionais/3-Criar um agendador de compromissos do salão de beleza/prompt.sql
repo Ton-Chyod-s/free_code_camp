d55c271f3fdb2b76c9a9d3831192c599700791f4
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-31 08:11:59 -0400

    doc: att

--- 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/prompt.sql
+++ 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/prompt.sql
@@ -13,6 +13,7 @@ create table appointments(
   appointment_id serial primary key,
   customer_id int not null,
   service_id int not null,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
   name varchar not null,
   time varchar
 
@@ -20,6 +21,7 @@ create table appointments(
 
 create table services(
   service_id serial primary key,
-  name varchar not null
+  name varchar not null,
+  FOREIGN KEY (service_id) REFERENCES appointments(service_id)
 
 );
