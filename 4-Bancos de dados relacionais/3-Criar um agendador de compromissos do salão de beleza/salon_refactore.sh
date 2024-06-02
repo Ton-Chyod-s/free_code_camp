b521e1938201408ef239329019827487987c3b21
Klay <hix_x@hotmail.com>
2024-06-02 10:05:32 -0400

    doc: att

--- 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/salon_refactore.sh
+++ 4-Bancos de dados relacionais/3-Criar um agendador de compromissos do salão de beleza/salon_refactore.sh
@@ -36,21 +36,23 @@ insert_customer() {
     echo -e "\nWhat's your phone number?"
     read CUSTOMER_PHONE
 
-    CUSTOMER_NAME=$($PSQL "select name from customers where = '$CUSTOMER_PHONE';")
+    CUSTOMER_NAME=$($PSQL "select name from customers where phone = '$CUSTOMER_PHONE';")
 
     local selection=$1
     local SERVICE_ID_SELECTED=$(echo "$SERVICES" | awk -F"|" -v sel="$selection" '$1 == sel {print $1}')
+    local SERVICE_NAME_SELECTED=$(echo "$SERVICES" | awk -F"|" -v sel="$selection" '$1 == sel {print $2}')
 
     if [[ -z $CUSTOMER_NAME ]]
     then
       echo -e "\nI don't have a record for that phone number, what's your name?"
       read CUSTOMER_NAME
     fi
-    
 
+    echo -e "\nWhat time would you like your $SERVICE_NAME_SELECTED, $CUSTOMER_NAME?"
+    read SERVICE_TIME
    
-    # Insert the new customer into the customers table
-    customer_id=$($PSQL "INSERT INTO customers(service_id, name, phone) VALUES ($SERVICE_ID_SELECTED, '$name', '$phone') RETURNING customer_id;")
+    customer_id=$($PSQL "INSERT INTO customers(service_id, name, phone, service_time) VALUES ($SERVICE_ID_SELECTED, '$CUSTOMER_NAME', '$CUSTOMER_PHONE', '$SERVICE_TIME') RETURNING customer_id;")
+    
 }
 
 MAIN_MENU
