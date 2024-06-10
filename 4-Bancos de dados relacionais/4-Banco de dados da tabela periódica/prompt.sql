b3588e0e7e715c6a7bd93b429e6399eea6fddf3a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 08:12:09 -0400

    doc: att

--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/prompt.sql
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/prompt.sql
@@ -13,4 +13,11 @@ alter table properties alter column boiling_point_celsius set not null;
 alter table elements add constraint unique_name UNIQUE(name);
 alter table elements add constraint unique_symbol UNIQUE(symbol);
 
+alter table elements alter column name set not null;
+alter table elements alter column symbol set not null;
+
+alter table properties ADD CONSTRAINT fk_atomic_number foreign key (atomic_number) references elements(atomic_number);
+
+create table types( type_id int primary key, type varchar not null);
+
 */
