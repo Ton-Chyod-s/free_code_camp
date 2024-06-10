bc74dcb02962d86b3b3366d5cbb939d102842188
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 10:37:15 -0400

    doc: att

--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
@@ -6,7 +6,17 @@ echo 'Please provide an element as an argument.'
 
 ELEMENT="$1"
 
-echo $ELEMENT
+if [[ -z $ELEMENT ]]; then
+  echo 'No element provided.'
+  exit 1
+fi
 
+if [[ $ELEMENT =~ [0-118] ]]; then
+  QUERY=$($PSQL -c "SELECT * FROM elements WHERE atomic_number = $ELEMENT;")
+else
+  QUERY=$($PSQL -c "SELECT * FROM elements WHERE symbol = '$ELEMENT';")
+fi
+
+echo $QUERY
 
 # echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
