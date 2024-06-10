7b90dd6f4785684d11df842a21af6b999b273978
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 10:53:00 -0400

    doc: att

--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
@@ -3,20 +3,17 @@
 PSQL="psql -U postgres -t --no-align --dbname=periodic_table"
 
 echo 'Please provide an element as an argument.'
-
 ELEMENT="$1"
 
-if [[ -z $ELEMENT ]]; then
-  echo 'No element provided.'
-  exit 1
+if [[ ! -z $ELEMENT ]]; then
+  if [[ $ELEMENT =~ ^(0|[1-9][0-9]?|1[01][0-8])$ ]]; then
+    QUERY=$($PSQL -c "SELECT atomic_number FROM elements WHERE atomic_number = $ELEMENT;")
+    
+  else
+    QUERY=$($PSQL -c "SELECT atomic_number FROM elements WHERE symbol = '$ELEMENT';")
+ 
+  fi
+
+  echo "The element with atomic number $QUERY is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
 fi
 
-if [[ $ELEMENT =~ [0-118] ]]; then
-  QUERY=$($PSQL -c "SELECT * FROM elements WHERE atomic_number = $ELEMENT;")
-else
-  QUERY=$($PSQL -c "SELECT * FROM elements WHERE symbol = '$ELEMENT';")
-fi
-
-echo $QUERY
-
-# echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
