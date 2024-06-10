497c7664dbab8160ffebf0dbc58463257248d6f3
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 08:04:33 -0400

    doc: att

--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/prompt.sql
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/prompt.sql
@@ -1,5 +1,16 @@
 /*
-
 touch element.sh
+
+\c periodic_table
+
+alter table properties rename column weight to atomic_mass;
+alter table properties rename column melting_point to melting_point_celsius;
+alter table properties rename column boiling_point to boiling_point_celsius;
+
+alter table properties alter column melting_point_celsius set not null;
+alter table properties alter column boiling_point_celsius set not null;
+
+alter table elements add constraint unique_name UNIQUE(name);
+alter table elements add constraint unique_symbol UNIQUE(symbol);
 
 */
--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/queries.sh
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/queries.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
 PSQL="psql -U postgres -t --no-align --dbname=periodic_table"
-
