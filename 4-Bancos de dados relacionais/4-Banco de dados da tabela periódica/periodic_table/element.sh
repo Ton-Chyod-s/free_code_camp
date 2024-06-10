f5cd51ac115b6a6b246447eff03a9a75b9ae0361
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 11:26:23 -0400

    doc: att

--- 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
+++ 4-Bancos de dados relacionais/4-Banco de dados da tabela periódica/periodic_table/element.sh
@@ -18,9 +18,12 @@ if [[ ! -z $ELEMENT ]]; then
       ATOMIC_NUMBER=$($PSQL -c "SELECT atomic_number FROM elements WHERE symbol = '$ELEMENT';")
     fi
   fi
-  
-  NAME_ELEMENT=$($PSQL -c "select name from elements where atomic_number = $ATOMIC_NUMBER")
 
-  echo "The element with atomic number $ATOMIC_NUMBER is $NAME_ELEMENT (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+  NAME_ELEMENT=$($PSQL -c "select name from elements where atomic_number = $ATOMIC_NUMBER")
+  SYMBOL_ELEMENT=$($PSQL -c "select symbol from elements where atomic_number = $ATOMIC_NUMBER")
+
+
+
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME_ELEMENT ($SYMBOL_ELEMENT). It's a nonmetal, with a mass of 1.008 amu. $NAME_ELEMENT has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
 fi
 
