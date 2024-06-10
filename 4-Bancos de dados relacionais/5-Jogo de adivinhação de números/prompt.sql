0f5d63473bf84137bc996c35044635d9a226c322
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-10 16:57:23 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/prompt.sql
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/prompt.sql
@@ -1,5 +1,18 @@
 /*
 
+mkdir number_guessing_game
+
+number_guess.sh
+
+chmod +x number_guess.sh
+
+git init 
+
+git add .
+
+git commit -m "Initial commit"
+
+git commit -m "feat: start game"
 
 
 
--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 PSQL="psql -U postgres -t --no-align --dbname=periodic_table"
+
+ELEMENT=$(initcap "$1")
