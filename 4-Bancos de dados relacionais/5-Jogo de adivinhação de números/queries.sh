fa6b21b2233292239717633e8bab36e13d2bcc21
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-11 10:56:01 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
@@ -24,12 +24,8 @@ fi
 echo -e "\nGuess the secret number between 1 and 1000:"
 read SECRET_NUMBER
 
-
-
-# echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
-
-if [[ $ELEMENT =~ ^(0|[1-9][0-9]?|1[01][0-8])$ ]]; then
-  if [[ $SECRET_NUMBER -eq $NUM_RANDOM ]]; then
+if [[ $SECRET_NUMBER =~ ^-?[0-9]+$ ]]; then
+  if [[ $SECRET_NUMBER < $NUM_RANDOM ]]; then
       echo "It's lower than that, guess again:"
   else
       echo "It's higher than that, guess again:"
@@ -37,3 +33,6 @@ if [[ $ELEMENT =~ ^(0|[1-9][0-9]?|1[01][0-8])$ ]]; then
 else
   echo "That is not an integer, guess again:"
 fi
+
+
+# echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
