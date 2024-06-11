3eb7d316ac42da600571a45e6cda63fa073eadb5
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-11 09:46:08 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
@@ -7,26 +7,36 @@ NUM_RANDOM=$RANDOM
 echo "Enter your username:"
 read NAME
 
-ID_NAME=$($PSQL -c "SELECT id_name FROM players WHERE name = '$NAME';")
+ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
 
-GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;")
+if [[ -z $ID_NAME ]]; then
+  echo "Welcome, $NAME! It looks like this is your first time here."
+  INSERT_NAME=$($PSQL "insert into name (nome) values ('$NAME') returning id_name;")
 
-BEST_GAME=$($PSQL -c "SELECT max(tentativa) FROM game WHERE id_name = $ID_NAME;")
+  echo "Guess the secret number between 1 and 1000:"
 
-if [[ ! -z $ID_NAME ]]; then
-    echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+else
+  GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;")
+
+  BEST_GAME=$($PSQL -c "SELECT max(tentativa) FROM game WHERE id_name = $ID_NAME;")
+
+  echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 
 
-echo "Welcome, $NAME! It looks like this is your first time here."
 
-echo "Guess the secret number between 1 and 1000:"
 
-echo "It's lower than that, guess again:"
 
-echo "It's higher than that, guess again:"
 
-echo "That is not an integer, guess again:"
 
-echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
+
+
+
+# echo "It's lower than that, guess again:"
+
+# echo "It's higher than that, guess again:"
+
+# echo "That is not an integer, guess again:"
+
+# echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
