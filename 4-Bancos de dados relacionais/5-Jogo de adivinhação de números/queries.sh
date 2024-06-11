ac009fa245de400a4403abff370c2c43b479ba6a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-11 11:04:28 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 PSQL="psql -U postgres -t --no-align --dbname=number_guess"
 
@@ -7,32 +7,43 @@ NUM_RANDOM=$((RANDOM % 1000 + 1))
 echo "Enter your username:"
 read NAME
 
-ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
+function check_name {
+  ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
 
-if [[ -z $ID_NAME ]]; then
-  echo "Welcome, $NAME! It looks like this is your first time here."
-  INSERT_NAME=$($PSQL -c "insert into name(nome) values ('$NAME');")
-else
-  GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;")
-
-  BEST_GAME=$($PSQL -c "SELECT max(tentativa) FROM game WHERE id_name = $ID_NAME;")
-
-  echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-
-fi
-
-echo -e "\nGuess the secret number between 1 and 1000:"
-read SECRET_NUMBER
-
-if [[ $SECRET_NUMBER =~ ^-?[0-9]+$ ]]; then
-  if [[ $SECRET_NUMBER < $NUM_RANDOM ]]; then
-      echo "It's lower than that, guess again:"
+  if [[ -z $ID_NAME ]]; then
+    echo "Welcome, $NAME! It looks like this is your first time here."
+    INSERT_NAME=$($PSQL -c "INSERT INTO name(nome) VALUES ('$NAME');")
+    ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
   else
-      echo "It's higher than that, guess again:"
+    GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;")
+    BEST_GAME=$($PSQL -c "SELECT min(tentativa) FROM game WHERE id_name = $ID_NAME;")
+
+    echo "Welcome back, $NAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
-else
-  echo "That is not an integer, guess again:"
-fi
+}
 
+function play_game {
+  ATTEMPTS=0
+  while true; do
+    echo -e "\nGuess the secret number between 1 and 1000:"
+    read SECRET_NUMBER
 
-# echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
+    if [[ $SECRET_NUMBER =~ ^[0-9]+$ ]]; then
+      ((ATTEMPTS++))
+      if (( SECRET_NUMBER < NUM_RANDOM )); then
+        echo "It's higher than that, guess again:"
+      elif (( SECRET_NUMBER > NUM_RANDOM )); then
+        echo "It's lower than that, guess again:"
+      else
+        echo "You guessed it in $ATTEMPTS tries. The secret number was $NUM_RANDOM. Nice job!"
+        INSERT_GAME=$($PSQL -c "INSERT INTO game(id_name, tentativa) VALUES ($ID_NAME, $ATTEMPTS);")
+        break
+      fi
+    else
+      echo "That is not an integer, guess again:"
+    fi
+  done
+}
+
+check_name
+play_game
