c1edbd7c4e79909e8bec0807c07a3268f4918748
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-11 14:20:11 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/queries.sh
@@ -7,16 +7,16 @@ echo "Enter your username:"
 read NAME
 
 function check_name {
-  ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';" | xargs)
+  ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
 
   if [[ -z $ID_NAME ]]; then
     echo "Welcome, $NAME! It looks like this is your first time here."
-    $PSQL -c "INSERT INTO name(nome) VALUES ('$NAME');"
-    ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';" | xargs)
+    INSERT_NAME=$($PSQL -c "INSERT INTO name(nome) VALUES ('$NAME');")
+    ID_NAME=$($PSQL -c "SELECT id_name FROM name WHERE nome = '$NAME';")
   else
-    NAME_DATABASE=$($PSQL -c "SELECT nome FROM name WHERE id_name = $ID_NAME;" | xargs)
-    GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;" | xargs)
-    BEST_GAME=$($PSQL -c "SELECT min(tentativa) FROM game WHERE id_name = $ID_NAME;" | xargs)
+    NAME_DATABASE=$($PSQL -c "SELECT nome FROM name WHERE id_name = $ID_NAME;")
+    GAMES_PLAYED=$($PSQL -c "SELECT count(tentativa) FROM game WHERE id_name = $ID_NAME;")
+    BEST_GAME=$($PSQL -c "SELECT min(tentativa) FROM game WHERE id_name = $ID_NAME;")
 
     echo "Welcome back, $NAME_DATABASE! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
@@ -31,11 +31,11 @@ function play_game {
     if [[ $SECRET_NUMBER =~ ^[0-9]+$ ]]; then
       ((ATTEMPTS++))
       if (( SECRET_NUMBER > NUM_RANDOM )); then
-        echo "It's lower than that, guess again:"
-      elif (( SECRET_NUMBER < NUM_RANDOM )); then
         echo "It's higher than that, guess again:"
+      elif (( SECRET_NUMBER < NUM_RANDOM )); then
+        echo "It's lower than that, guess again:"
       else
-        $PSQL -c "INSERT INTO game(numero, tentativa, id_name) VALUES ($NUM_RANDOM, $ATTEMPTS, $ID_NAME);"
+        INSERT_GAME=$($PSQL -c "INSERT INTO game( numero, tentativa, id_name) VALUES ( $SECRET_NUMBER, $ATTEMPTS, $ID_NAME);")
         echo -e "\nYou guessed it in $ATTEMPTS tries. The secret number was $NUM_RANDOM. Nice job!\n"
         exit
       fi
