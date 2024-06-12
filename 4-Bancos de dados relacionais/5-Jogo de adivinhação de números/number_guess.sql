be9088cd2f98356c6bd8ce672a88cfec4c522c8e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-12 10:16:34 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess.sql
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess.sql
@@ -32,6 +32,10 @@ create table name( id_name serial primary key, username varchar(22) not null, cr
 
 create table game( id_game serial primary key, number_of_guesses int not null, guesses int not null, created_at DATE NOT NULL DEFAULT CURRENT_DATE, id_name int not null, foreign key (id_name) references name(id_name));
 
+git add .
+
+git commit -m "feat: Continue game"
+
 drop table game;
 drop table name;
 
