1fba0a945c6a886f2b97479730aea7803be99ecc
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-12 11:16:51 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess.sh
@@ -11,7 +11,7 @@ declare -i number_of_guesses=0
 CHECK_NAME=$($PSQL -c "SELECT username FROM name WHERE username = '$username';" | xargs)
 
 if [[ ! -z $CHECK_NAME ]]; then
-  games_played=$($PSQL -c "select count(guesses) from game join name on game.id_name = name.id_name where username = '$username';" | xargs)
+  games_played=$($PSQL -c "select count(*) from game join name on game.id_name = name.id_name where username = '$username';" | xargs)
   if [[ -z $games_played ]]; then
     games_played=0
   fi
@@ -20,7 +20,7 @@ if [[ ! -z $CHECK_NAME ]]; then
   if [[ -z $best_game ]]; then
     best_game=0
   fi
-  
+
   echo "Welcome back, $CHECK_NAME! You have played $games_played games, and your best game took $best_game guesses."
 else
   echo "Welcome, $username! It looks like this is your first time here."
@@ -42,7 +42,7 @@ while true; do
       echo "It's lower than that, guess again:"
     else
       INSERT_GAME=$($PSQL -c "INSERT INTO game( number_of_guesses, guesses, id_name) VALUES ( $secret_number, $number_of_guesses, $ID_NAME);")
-      echo -e "\nYou guessed it in $number_of_guesses tries. The secret number was $secret_number. Nice job!\n"
+      echo -e "\nYou guessed it in $number_of_guesses tries. The secret number was $NUM_RANDOM. Nice job!\n"
       exit
     fi
   else
