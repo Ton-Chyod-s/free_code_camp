42f00869ad2162fd33f8ef621e8125c6519973bd
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-16 09:14:05 -0400

    doc: att

--- 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
+++ 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
@@ -1,7 +1,60 @@
 import pandas as pd
-import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_excel(os.path.abspath('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.xlsx'))
-print(df)
+# 1
+df = None
 
-#nova coluna
+# 2
+df['overweight'] = None
+
+# 3
+
+
+# 4
+def draw_cat_plot():
+    # 5
+    df_cat = None
+
+
+    # 6
+    df_cat = None
+    
+
+    # 7
+
+
+
+    # 8
+    fig = None
+
+
+    # 9
+    fig.savefig('catplot.png')
+    return fig
+
+
+# 10
+def draw_heat_map():
+    # 11
+    df_heat = None
+
+    # 12
+    corr = None
+
+    # 13
+    mask = None
+
+
+
+    # 14
+    fig, ax = None
+
+    # 15
+
+
+
+    # 16
+    fig.savefig('heatmap.png')
+    return fig
