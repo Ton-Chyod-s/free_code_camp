c0261671e244352ae17627954b43b3a8fc160dee
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-16 14:48:47 -0400

    doc: att

--- 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
+++ 2-Análise de dados com Python/3-Visualizador de dados médicos/medical_data_visualizer.py
@@ -6,33 +6,28 @@ import numpy as np
 # 1
 df = pd.read_csv('2-Análise de dados com Python\\3-Visualizador de dados médicos\\medical_examination.csv')
 
-# 2
-df['overweight'] = ''
+#2
+df['overweight'] = (df['weight'] / ((df['height'] / 100) ** 2) > 25).astype(int)
 
 #3
-for i in range(len(df)):
-    numColesterol = df['cholesterol'][i]
-    numGluc = df['gluc'][i]
+df['cholesterol'] = df['cholesterol'].map({1: 0, 2: 1, 3: 1})
+df['gluc'] = df['gluc'].map({1: 0, 2: 1, 3: 1})
 
-    if numColesterol == 1 or numGluc == 1:
-        df.iloc[[14, i]] = 1
-    else:
-        df.iloc[[14, i]] = 0
-        
 # 4
 def draw_cat_plot():
     # 5
-    cholesterol = df['cholesterol']
-    gluc = df['gluc']
-    smoke = df['smoke']
-    alco = df['alco']
-    active = df['active']
-    overweight = df['overweight']
+    
+    variables = ['cholesterol', 'gluc', 'smoker', 'alco', 'active', 'overweight']
+
+    # Create a FacetGrid with multiple plots
+    grafico = sns.catplot(data=df, kind='count', x='cardio', hue='overweight')
+        
+    grafico.set_ylabels('')
+    grafico.set_xlabels('')
 
 
 
     df_cat = None
-
 
     # 6
     df_cat = None
@@ -40,15 +35,15 @@ def draw_cat_plot():
 
     # 7
 
-
-
     # 8
     fig = None
 
 
     # 9
-    fig.savefig('catplot.png')
-    return fig
+    grafico.savefig('2-Análise de dados com Python\\3-Visualizador de dados médicos\\catplot.png')
+   
+
+    return grafico
 
 
 # 10
@@ -78,4 +73,4 @@ def draw_heat_map():
 if __name__ == "__main__":
     # 17
     draw_cat_plot()
-    draw_heat_map()
+    #draw_heat_map()
