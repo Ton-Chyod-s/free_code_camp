d189e73107c1b4e06fc7b40809881962c9a089a6
Klay <hix_x@hotmail.com>
2024-08-28 10:34:00 -0400

    doc: att

--- 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess_refactored.sh
+++ 4-Bancos de dados relacionais/5-Jogo de adivinhação de números/number_guess_refactored.sh
@@ -7,26 +7,28 @@ NUM_RANDOM=$((1 + RANDOM % 1000))
 echo "Enter your username:"
 read USER
 
-CHECK_USER=$($PSQL -c "select username from name where username = '$USER'")
+CHECK_USER=$($PSQL -c "select username from name where username = '$USER'" | xargs)
 
-CHECK_PLAYED=$($PSQL -c "select count(*) from game join name on game.id_name = name.id_name where username = '$username';" | xargs)
+CHECK_PLAYED=$($PSQL -c "select count(*) from game join name on game.id_name = name.id_name where username = '$USER';" | xargs)
 if [[ -z $CHECK_PLAYED ]]; then
   CHECK_PLAYED=0
 fi
 
-CHECK_BEST=$($PSQL -c "select min(guesses) from game join name on game.id_name = name.id_name where username = '$username';" | xargs)
+CHECK_BEST=$($PSQL -c "select min(guesses) from game join name on game.id_name = name.id_name where username = '$USER';" | xargs)
 if [[ -z $CHECK_BEST ]]; then
   CHECK_BEST=0
 fi
 
 if [[ -z $CHECK_USER ]]; then
   echo "Welcome, $USER! It looks like this is your first time here."
-  CHECK_USER=$($PSQL -c "INSERT INTO name(username) VALUES ('$USER');")
+  CHECK_USER=$($PSQL -c "INSERT INTO name(username) VALUES ('$USER');" | xargs)
   ID_NAME=$($PSQL -c "SELECT id_name FROM name where username = '$USER';" | xargs)
 else
   echo "Welcome back, $CHECK_USER You have played $CHECK_PLAYED games, and your best game took $CHECK_BEST guesses."
   ID_NAME=$($PSQL -c "SELECT id_name FROM name where username = '$CHECK_USER';" | xargs)
 fi
+
+number_of_guesses=0
 
 while true; do
   echo "Guess the secret number between 1 and 1000:"
